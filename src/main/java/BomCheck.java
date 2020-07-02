78a23d54329370cc7c14137989a89ec5d47bf538
Alexey Mukas <alexey.mukas@gmail.com>
2020-07-02 15:00:37 +0200

    initial commit

---
+++ src/main/java/BomCheck.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class BomCheck {
+    //export LC_ALL=en_US.UTF-8
+    public static void main(String[] args) throws Exception {
+        byte[] withBom = Files.readAllBytes(Paths.get("test-data.txt"));
+        System.out.println("current encoding: " + Charset.defaultCharset());
+        String withOutBom = "<xml>test</xml>\n";
+        String str = new String(withBom, StandardCharsets.UTF_8).replaceAll("[\uFEFF-\uFFFF]", "");
+        System.out.println(str);
+        System.out.println("withOutBom.equals(str): " + withOutBom.equals(str));
+    }
+}
